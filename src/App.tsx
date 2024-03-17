b7fbf28e9f6c4720d826e580168cc642369273d4
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-03-17 14:06:13 +0900

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <>
         <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
+            <Route path="/" element={<Login/>}></Route>
+            {/*<Route path="/login" element={<Login />}></Route>*/}
         </Routes>
     </>
   )
