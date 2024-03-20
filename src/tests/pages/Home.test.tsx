f34498d54dbcdc8128a4f6227c8adb59df44cc33
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2024-03-20 21:31:03 +0900

    Home.tsxのテスト修正。特定の引数だけstubに変更した

--- src/tests/pages/Home.test.tsx
+++ src/tests/pages/Home.test.tsx
@@ -1,95 +1,107 @@
-import {vi} from "vitest";
-import {render, waitFor} from "@testing-library/react";
-import {Home} from "../../pages/Home.tsx";
-import {accessTokenGetService, checkAccessTokenService} from "../../service/AuthService.ts";
-import {useNavigate} from "react-router-dom";
+import {vi} from 'vitest';
+import {render, waitFor} from '@testing-library/react';
+import {Home} from '../../pages/Home.tsx';
+import {
+    accessTokenGetService,
+    checkAccessTokenService, checkIDTokenService,
+} from '../../service/AuthService.ts';
+import {useNavigate} from 'react-router-dom';
 
-vi.mock("../../service/AuthService.ts")
-vi.mock('react-router-dom')
+vi.mock('../../service/AuthService.ts');
+vi.mock('react-router-dom');
 
-const originalLocalStorage = window.localStorage
-const originalURLParams = URLSearchParams
-const originalLocation = window.location
+const originalLocalStorage = window.localStorage;
+const originalURLParams = URLSearchParams;
+const originalLocation = window.location;
 
-describe('Home.tsxのテスト',()=>{
+describe('Home.tsxのテスト', () => {
     afterEach(() => {
-        vi.restoreAllMocks()
-        // vi.resetModules();
-        Object.defineProperty(window, "localStorage", {
+        vi.restoreAllMocks();
+        Object.defineProperty(window, 'localStorage', {
             value: originalLocalStorage,
-        })
-        Object.defineProperty(window, "location", {
+        });
+        Object.defineProperty(window, 'location', {
             value: originalLocation,
-        })
-        Object.defineProperty(window, "URLSearchParams", {
-            value: originalURLParams
-        })
-    })
+        });
+        Object.defineProperty(window, 'URLSearchParams', {
+            value: originalURLParams,
+        });
+    });
 
-    describe('クエリパラメーターのチェックテスト',()=>{
-        test('クエリパラメーターにcodeが含まれている場合、accessTokenGetServiceを正しい引数で呼ぶ',()=>{
-
-            const spyUrlQuery = vi.fn()
+    describe('クエリパラメーターのチェックテスト', () => {
+        test('クエリパラメーターにcodeが含まれている場合、accessTokenGetServiceを正しい引数で呼ぶ', async () => {
+            const spyURLSearchParams = vi.fn();
             const mockSearchParams = {
-                get: spyUrlQuery.mockReturnValue('abc'),
+                get: vi.fn().mockImplementation((params) =>
+                    params === 'code' ? 'abc' : null
+                ),
             };
 
-            //location.searchのプロパティ値をモックする必要は今回のテストではないが、このようにできることも書いておく
-            vi.stubGlobal('location', { search: 'mocked_search_string' });
-            vi.stubGlobal('URLSearchParams', vi.fn().mockImplementation(() => mockSearchParams));
-            vi.mocked(useNavigate).mockReturnValue(()=>{})
-
-
-            render(<Home setUserId={()=>{}} />)
-
-            expect(spyUrlQuery).toHaveBeenCalledWith('code')
-            expect(accessTokenGetService).toHaveBeenCalledWith('abc')
-
-        })
-    })
-
-    describe('checkAccessTokenServiceに関するテスト',()=>{
-
-        test('レンダーされるとcheckAccessTokenServiceを正しい引数で呼ぶ',async ()=>{
-            Object.defineProperty(window, 'localStorage',{
-                value : {
-                    getItem : () => 'test-access-token'
-                }
+            vi.stubGlobal('location', {search: 'mocked_search_string'});
+            vi.stubGlobal(
+                'URLSearchParams',
+                spyURLSearchParams.mockReturnValue(mockSearchParams)
+            );
+            vi.stubGlobal('localStorage', {
+                getItem: vi.fn().mockImplementation(params => params === 'id_token' ? 'test-id-token' : null)
             })
-            vi.mocked(useNavigate).mockReturnValue(()=>{})
+            vi.mocked(useNavigate).mockReturnValue(() => {
+            });
 
-            render(<Home setUserId={()=>{}} />)
+            render(<Home setUserId={() => {
+            }}/>);
 
-            expect(checkAccessTokenService).toHaveBeenCalledWith('test-access-token')
-        })
+            expect(spyURLSearchParams).toHaveBeenCalledWith('mocked_search_string');
+            expect(accessTokenGetService).toHaveBeenCalledWith('abc');
+            //実装側でaccessTokenGetServiceをawaitしてから実行するので、こっちの結果は待たないと、呼び出されない
+            await waitFor(
+                ()=> expect(checkIDTokenService).toHaveBeenCalledWith('test-id-token')
+            )
 
-        test('checkAccessTokenServiceの返り値がundefinedでない場合、propsのsetUserIdに返り値のsubプロパティを引数として呼び、useNavigate()に/userpageを引数として呼ぶ',async ()=>{
+        });
+    });
+
+    describe('checkAccessTokenServiceに関するテスト', () => {
+        test('レンダーされるとcheckAccessTokenServiceを正しい引数で呼ぶ', async () => {
+            Object.defineProperty(window, 'localStorage', {
+                value: {
+                    getItem: () => 'test-access-token',
+                },
+            });
+            vi.mocked(useNavigate).mockReturnValue(() => {
+            });
+
+            render(<Home setUserId={() => {
+            }}/>);
+
+            expect(checkAccessTokenService).toHaveBeenCalledWith('test-access-token');
+        });
+
+        test('checkAccessTokenServiceの返り値がundefinedでない場合、propsのsetUserIdに返り値のsubプロパティを引数として呼び、useNavigate()に/userpageを引数として呼ぶ', async () => {
             vi.mocked(checkAccessTokenService).mockResolvedValue({
-                sub:'test-user-data'
-            })
-            const spyNavigate = vi.fn()
-            vi.mocked(useNavigate).mockReturnValue(spyNavigate)
-            const spySetUserId = vi.fn()
+                sub: 'test-user-data',
+            });
+            const spyNavigate = vi.fn();
+            vi.mocked(useNavigate).mockReturnValue(spyNavigate);
+            const spySetUserId = vi.fn();
 
-            render(<Home setUserId={spySetUserId} />)
+            render(<Home setUserId={spySetUserId}/>);
 
-            await waitFor(
-                ()=> expect(spySetUserId).toHaveBeenCalledWith('test-user-data')
-            )
-            expect(spyNavigate).toHaveBeenCalledWith('/userpage')
-        })
+            await waitFor(() =>
+                expect(spySetUserId).toHaveBeenCalledWith('test-user-data')
+            );
+            expect(spyNavigate).toHaveBeenCalledWith('/userpage');
+        });
 
-        test('checkAccessTokenServiceの返り値がundefinedの場合、useNavigate()に/loginを引数として呼ぶ',async ()=>{
-            vi.mocked(checkAccessTokenService).mockResolvedValue(undefined)
-            const spyNavigate = vi.fn()
-            vi.mocked(useNavigate).mockReturnValue(spyNavigate)
+        test('checkAccessTokenServiceの返り値がundefinedの場合、useNavigate()に/loginを引数として呼ぶ', async () => {
+            vi.mocked(checkAccessTokenService).mockResolvedValue(undefined);
+            const spyNavigate = vi.fn();
+            vi.mocked(useNavigate).mockReturnValue(spyNavigate);
 
-            render(<Home setUserId={()=>{}} />)
+            render(<Home setUserId={() => {
+            }}/>);
 
-            await waitFor(
-                () => expect(spyNavigate).toHaveBeenCalledWith('/login')
-            )
-
-        })
-    })
-})
+            await waitFor(() => expect(spyNavigate).toHaveBeenCalledWith('/login'));
+        });
+    });
+});
